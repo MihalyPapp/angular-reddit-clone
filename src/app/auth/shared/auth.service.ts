77564ebe0b70286d1d47f6105d0bb623a57163f7
MihalyPapp <mispaaa@gmail.com>
2021-02-01 11:24:54 +0100

    initial

---
+++ src/app/auth/shared/auth.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SignupRequestPayload } from '../signup/signup-request.payload';
+import { Observable, of } from 'rxjs';
+import { LoginRequestPayload } from '../login/login-request.payload';
+import { LoginResponsePayload } from '../login/login-response.payload';
+import { LocalStorageService } from 'ngx-webstorage';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  refreshTokenPayload = {
+    token: this.getRefreshToken(),
+    username: this.getUsername()
+  }
+
+  constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) { }
+
+  signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
+    return this.httpClient.post('http://localhost:8844/api/auth/signup', signupRequestPayload, { responseType: 'text' })
+  }
+
+  login(LoginRequestPayload: LoginRequestPayload): Observable<any> {
+    return this.httpClient.post<LoginResponsePayload>('http://localhost:8844/api/auth/login', LoginRequestPayload)
+      .pipe(
+        map(data => {
+          this.localStorage.store('authenticationToken', data.authenticationToken)
+          this.localStorage.store('expiresAt', data.expiresAt)
+          this.localStorage.store('refreshToken', data.refreshToken)
+          this.localStorage.store('username', data.username)
+          console.log("login request -> storing token") //////////////////////////////////////
+          return true;
+        }),
+        catchError(() => {
+          return of(false)
+        })
+      )
+  }
+
+  getJwtToken() {
+    console.log("getJwtToken from localstorage")
+    return this.localStorage.retrieve('authenticationToken')
+  }
+
+  refreshToken() {
+    return this.httpClient.post<LoginResponsePayload>('http://localhost:8844/api/auth/refresh/token', this.refreshTokenPayload)
+      .pipe(tap(response => {
+        this.localStorage.clear('authenticationToken');
+        this.localStorage.clear('expiresAt');
+        
+        this.localStorage.store('authenticationToken', response.authenticationToken)
+        this.localStorage.store('expiresAt', response.expiresAt)
+      }))
+  }
+  getUsername() {
+    return this.localStorage.retrieve('username')
+  }
+  getRefreshToken() {
+    return this.localStorage.retrieve('refreshToken')
+  }
+
+}
---
+++ src/app/token-interceptor.ts
@@ -0,0 +1,62 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, throwError } from "rxjs";
+import { catchError, filter, switchMap, take } from "rxjs/operators";
+import { LoginResponsePayload } from "./auth/login/login-response.payload";
+import { AuthService } from "./auth/shared/auth.service";
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+    isTokenRefreshing = false;
+    refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject(null)
+
+    constructor(private authService: AuthService) { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        /*if (request.url.indexOf('refresh') !== -1 || request.url.indexOf('login') !== -1)
+            return next.handle(request);*/
+
+        const jwtToken = this.authService.getJwtToken()
+        console.log("token: " + jwtToken)
+
+        if (jwtToken) {
+            return next.handle(this.addToken(request, jwtToken)).pipe(catchError(error => {
+                if (error instanceof HttpErrorResponse && error.status === 403)
+                    return this.handleAuthErrors(request, next)
+                else
+                    return throwError(error)
+            }))
+        }
+        return next.handle(request)
+    }
+
+    addToken(request: HttpRequest<any>, jwtToken: any) {
+        return request.clone({
+            headers: request.headers.set('Authorization','Bearer ' + jwtToken)
+        })
+    }
+
+    private handleAuthErrors(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (!this.isTokenRefreshing) {
+            this.isTokenRefreshing = true;
+            this.refreshTokenSubject.next(null)
+            return this.authService.refreshToken().pipe(
+                switchMap((refreshTokenResponse: LoginResponsePayload) => {
+                    this.isTokenRefreshing = false
+                    this.refreshTokenSubject.next(refreshTokenResponse.authenticationToken)
+                    return next.handle(this.addToken(request, refreshTokenResponse.authenticationToken))
+                })
+            )
+        } else {
+            return this.refreshTokenSubject.pipe(
+                filter(result => result !== null),
+                take(1),
+                switchMap(() => {
+                    return next.handle(this.addToken(request, this.authService.getJwtToken()))
+                })
+            )
+        }
+    }
+
+}
